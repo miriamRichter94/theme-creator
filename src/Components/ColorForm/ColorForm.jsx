4e382fc70f6ad80b0ccada401480f04376524329
miriamRichter94 <dark.rose2094@gmail.com>
2026-03-09 10:19:23 +0100

    Adding a Formular to add a Color-Set to the  array

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,19 +1,55 @@
+import { useState } from "react";
 import "./ColorForm.css";
 
-export default function ColorForm() {
+export default function ColorForm({ onAddColor }) {
+  const [hexColor, setHexColor] = useState("#000000");
+  const [contrast, setContrast] = useState("#ffffff");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    onAddColor(data);
+    event.target.reset();
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <label htmlFor="role">Role</label>
-          <input type="text" id="role" />
+          <input type="text" name="role" id="role" required />
           <label htmlFor="hex">Hex</label>
-          <input type="text" placeholder="Hexa" />
-          <input type="color" id="hex" />
+          <input
+            type="text"
+            name="hex"
+            placeholder="Hexa"
+            value={hexColor}
+            onChange={(event) => setHexColor(event.target.value)}
+          />
+          <input
+            type="color"
+            id="hex"
+            onChange={(event) => setHexColor(event.target.value)}
+            value={hexColor}
+          />
           <label htmlFor="contrast-text">Contrast Text</label>
-          <input type="text" placeholder="contrast-text" />
-          <input type="color" id="contrast-text" />
+          <input
+            type="text"
+            name="contrastText"
+            placeholder="contrast-text"
+            value={contrast}
+            onChange={(event) => setContrast(event.target.value)}
+          />
+          <input
+            type="color"
+            id="contrast-text"
+            onChange={(event) => setContrast(event.target.value)}
+            value={contrast}
+          />
         </fieldset>
+        <button type="submit">ADD COLOR</button>
       </form>
     </>
   );
